0daf1e55d226915e1ae55e6f69af94445fab91c7
Rodrigo Zea <zearodrigo37@gmail.com>
2019-09-13 23:06:24 -0600

    Configuracion inicial de webpack + babel + react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/*const App = () => {
+    return <div>Hello React,Webpack 4 & Babel 7!</div>;
+};*/
+
+function App() {
+    return <div>hola</div>;
+};
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        },
+        {
+            test: /\.html$/,
+            use: [
+                {
+                    loader: "html-loader"
+                }
+            ]
+        }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
